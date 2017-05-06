7c49b001da280ce885e58205a7d154cd63b46906
stephanos48 <stephanos.english@gmail.com>
2017-05-06 16:27:53 -0500

    initial

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(persephone.Startup))]
+namespace persephone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
